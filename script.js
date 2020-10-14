4da4ee196b578afcf94e827c472fdcc714e14736
DragosR2020 <70966753+DragosR2020@users.noreply.github.com>
2020-10-14 12:03:28 +0100

    Add files via upload
    My first JS code/ exercise.

---
+++ script.js
@@ -0,0 +1,24 @@
+function day(){
+  document.body.style.background = "url('day.jpg')";
+  document.body.style.backgroundRepeat = "no-repeat";
+  document.body.style.backgroundSize = "cover";
+  document.body.style.backgroundPosition = "center";
+  document.getElementById("window1").style.backgroundColor = "white";
+  document.getElementById("window2").style.backgroundColor = "lightblue";
+  document.getElementById("window3").style.backgroundColor = "lightblue";
+  document.getElementById("window4").style.backgroundColor = "white";
+  document.getElementById("window5").style.backgroundColor = "white";
+  document.getElementById("window6").style.backgroundColor = "lightblue";
+}
+function night(){
+  document.body.style.background = "url('night.jpg')";
+  document.body.style.backgroundRepeat = "no-repeat";
+  document.body.style.backgroundSize = "cover";
+  document.body.style.backgroundPosition = "center";
+  document.getElementById("window1").style.backgroundColor = "yellow";
+  document.getElementById("window2").style.backgroundColor = "orange";
+  document.getElementById("window3").style.backgroundColor = "orange";
+  document.getElementById("window4").style.backgroundColor = "yellow";
+  document.getElementById("window5").style.backgroundColor = "yellow";
+  document.getElementById("window6").style.backgroundColor = "orange";
+}
